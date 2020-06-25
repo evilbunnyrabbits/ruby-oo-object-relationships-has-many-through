325e0cc812812026ffdd561a4d363c45143d317d
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-06-25 17:08:16 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+require 'meal.rb'
+
+
 class Customer
-  
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,33 @@
 class Meal
 
+  @@all =[]
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def waiter
+    @waiter
+  end
+
+  def customer
+    @customer
+  end
+
+  def total
+    @total
+  end
+
+  def tip
+    @tip
+  end
+
+  def self.all
+    @@all
+  end
+
 end
